f939274a2d479413da9f7c2af9e64fcfcb935847
Adam Wilson <awilson@pubsvs.com>
2014-12-01 11:24:07 -0500

    output file

--- src/Driver.java
+++ src/Driver.java
@@ -1,54 +1,71 @@
+import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Driver {
 
 	public static final void main(String[] args) {
-		
+
 		long startTime;
 		long endTime;
 		long runTime;
 		ArrayList<Integer> integers;
-		
-		integers = loadArray();
-		startTime = System.currentTimeMillis();
-		InsertionSort.insertionSort(integers);
-		endTime = System.currentTimeMillis();
-		runTime = (( endTime - startTime ));
-		System.out.println("Runtime for Insertion Sort was: " + runTime);
-		
-		integers = loadArray();
-		startTime = System.currentTimeMillis();
-		QuickSort.quicksort(integers);
-		endTime = System.currentTimeMillis();
-		runTime = (( endTime - startTime ));
-		System.out.println("Runtime for Quick Sort was: " + runTime);
-		
-		integers = loadArray();
-		startTime = System.currentTimeMillis();
-		MergeSort.mergeSort(integers);
-		endTime = System.currentTimeMillis();
-		runTime = (( endTime - startTime ));
-		System.out.println("Runtime for Merge Sort was: " + runTime);
-		
+
+		try {
+			PrintStream out = new PrintStream(
+					new FileOutputStream("output.txt"));
+			System.setOut(out);
+
+			integers = loadArray();
+			startTime = System.currentTimeMillis();
+			InsertionSort.insertionSort(integers);
+			endTime = System.currentTimeMillis();
+			runTime = ((endTime - startTime));
+			System.out.println("Runtime for Insertion Sort was: " + runTime);
+
+			integers = loadArray();
+			startTime = System.currentTimeMillis();
+			QuickSort.quicksort(integers);
+			endTime = System.currentTimeMillis();
+			runTime = ((endTime - startTime));
+			System.out.println("Runtime for Quick Sort was: " + runTime);
+
+			integers = loadArray();
+			startTime = System.currentTimeMillis();
+			MergeSort.mergeSort(integers);
+			endTime = System.currentTimeMillis();
+			runTime = ((endTime - startTime));
+			System.out.println("Runtime for Merge Sort was: " + runTime);
+
+			integers = loadArray();
+			startTime = System.currentTimeMillis();
+			HeapSort.heapSort(integers);
+			endTime = System.currentTimeMillis();
+			runTime = ((endTime - startTime));
+			System.out.println("Runtime for Heap Sort was: " + runTime);
+		} catch (Exception ex) {
+			System.out.println("Error -- " + ex);
+		}
+
 	}
-	
-	public static ArrayList<Integer> loadArray(){
-		
+
+	public static ArrayList<Integer> loadArray() {
+
 		ArrayList<Integer> integers = new ArrayList<Integer>();
-		
+
 		try {
 			FileReader file = new FileReader("random-ints.txt");
-			
+
 			Scanner sc = new Scanner(file);
-			while(sc.hasNextInt()){
+			while (sc.hasNextInt()) {
 				integers.add(sc.nextInt());
 			}
 			sc.close();
-			
-		} catch (Exception e) {
 
+		} catch (Exception e) {
+			System.out.println("Error -- " + e);
 		}
 		return integers;
 	}
